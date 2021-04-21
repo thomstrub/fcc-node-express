b52f908ec9408af08d0f307c2cbf45469465c00a
Thomas Strub <thomstrub@gmail.com>
2021-04-21 16:54:22 -0700

    serves basic json

--- myApp.js
+++ myApp.js
@@ -2,13 +2,15 @@ var express = require('express');
 var app = express();
 
 console.log("Hello World");
-
-
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 app.get('/', function(req, res){
     res.sendFile(__dirname + "/views/index.html");
 })
+app.get('/json', (req, res) => {
+    res.json({"message": "Hello json"});
+});
 
-app.use(__dirname + "/public", express.static());
 
 
 
