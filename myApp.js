29c5523ad5af357d3c5ec64541247c2678d9db66
Thomas Strub <thomstrub@gmail.com>
2021-04-21 16:32:22 -0700

    adds express.static() middleware

--- myApp.js
+++ myApp.js
@@ -4,9 +4,11 @@ var app = express();
 console.log("Hello World");
 
 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/views/index.html");
+})
 
-
-
+app.use(__dirname + "/public", express.static());
 
 
 
