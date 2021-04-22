015eb955295800b366a25bb455ef507d6d38f357
Thomas Strub <thomstrub@gmail.com>
2021-04-21 19:17:26 -0700

    has issues with .env

--- myApp.js
+++ myApp.js
@@ -2,19 +2,19 @@ var express = require('express');
 var app = express();
 
 console.log("Hello World");
-app.use(express.static(__dirname + "/public"));
-app.use("/public", express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
+// app.use("/public", express.static(__dirname + "/public"));
 app.get('/', function(req, res){
     res.sendFile(__dirname + "/views/index.html");
 })
 
 app.get('/json', (req, res) => {
     
-    process.env.MESSAGE_STYLE === "uppercase" 
+    process.env.MESSAGE_STYLE == "uppercase" 
         ? 
-        res.json({"message": process.env.MESSAGE_STYLE})
+        res.json({"message": "uppercase"})
         :
-        res.json({"message": process.env.MESSAGE_STYLE})
+        res.json({"message": "not"})
 });
 
 
